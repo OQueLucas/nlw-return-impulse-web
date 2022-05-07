806a02fcd6185fbda9f1eaf46563d8c897dc52bb
Que-Lucas <l_queiroz@outlook.com.br>
2022-05-07 16:33:03 -0300

    first commit

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,10 @@
+import { Popover } from "@headlessui/react";
+import { X } from "phosphor-react";
+
+export function CloseButton() {
+  return (
+    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar formulário de feedback">
+      <X weight="bold" className="w4 h4"></X>
+    </Popover.Button>
+  );
+}
